2ea9b7aff8b3cbaafacbc96d95566d685295b248
Rowell Cruz <rcruz@blackberry.com>
2013-10-10 16:17:22 -0400

    Added /delete route to server
    Reviewed by Bryan Higgins <bhiggins@blackberry.com>

---
+++ test/unit/delete.spec.js
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2013 BlackBerry Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var root = __dirname + "/../../",
+    fs = require("fs"),
+    wrench = require("wrench"),
+    deleteAPI = require(root + "routes/delete"),
+    mockResponse = {
+        send: jasmine.createSpy()
+    };
+
+describe("delete", function () {
+    var pathToProject = "/path/to/project";
+
+    beforeEach(function () {
+        spyOn(console, "log");
+    });
+
+    afterEach(function () {
+        mockResponse.send.reset();
+    });
+
+    it("deletes given project path if it is valid", function () {
+        spyOn(fs, "existsSync").andReturn(true);
+        spyOn(wrench, "rmdirSyncRecursive");
+
+        deleteAPI.get({
+            query: {
+                path: pathToProject
+            }
+        }, mockResponse);
+
+        expect(fs.existsSync).toHaveBeenCalled();
+        expect(wrench.rmdirSyncRecursive).toHaveBeenCalled();
+        expect(mockResponse.send).toHaveBeenCalledWith(200, jasmine.any(Object));
+    });
+
+    it("sends 500 if given project path is not valid", function () {
+        spyOn(fs, "existsSync").andReturn(false);
+        spyOn(wrench, "rmdirSyncRecursive");
+
+        deleteAPI.get({
+            query: {
+                path: pathToProject
+            }
+        }, mockResponse);
+
+        expect(fs.existsSync).toHaveBeenCalled();
+        expect(wrench.rmdirSyncRecursive).not.toHaveBeenCalled();
+        expect(mockResponse.send).toHaveBeenCalledWith(500, jasmine.any(Object));
+    });
+});
+
