da56fce4e875380e3142860163f07a3557077363
Josh Soref <jsoref@blackberry.com>
2014-03-21 16:59:05 -0400

    Provide indication of stale platforms and ability to update them
    The refresh icon is derived from this public domain icon:
     http://publicdomainvectors.org/en/free-clipart/Eco-water-recycling-vector-icon/6856.html
     -- it's actually only inspired by, this art was drawn by me using InkScape

---
+++ routes/platform.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013 BlackBerry Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var fs = require("fs"),
+    cp = require("child_process"),
+    util = require("util"),
+    path = require("path"),
+    apiUtil = require("../lib/util");
+
+module.exports = {
+
+    get: function (req, res) {
+        var projectPath = req.query.path,
+            cmd = req.query.cmd,
+            args = req.query.args,
+            wwPath = path.resolve(__dirname, path.join("..", "..", "webworks")),
+            execStr,
+            child;
+
+        if (apiUtil.isValidProject(projectPath)) {
+            cp.exec('"' + wwPath + '"' + " platform "+cmd+" "+(args ? args.join(" ") : ""), { cwd: projectPath }, function (error, stdout, stderr) {
+                console.log("stdout: " + stdout);
+                console.log("stderr: " + stderr);
+
+                res.send(200, {
+                    success: !error,
+                    code: error ? error.code : 0,
+                    path: projectPath,
+                    args: args,
+                    stdout: stdout,
+                    stderr: stderr
+                });
+            });
+        } else {
+            res.send(500, { error: "'" + projectPath + "' does not exist or is missing .cordova or platforms/blackberry10/cordova" });
+        }
+    }
+};
